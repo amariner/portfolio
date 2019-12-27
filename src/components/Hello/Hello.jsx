a95993a01b9e865f9411276423875b0a528396cb
Andreu Mariner <info@andreumariner.com>
2019-12-27 15:51:16 +0100

    image

--- src/components/Hello/Hello.jsx
+++ src/components/Hello/Hello.jsx
@@ -5,10 +5,8 @@ import Underline from "../Underline/Underline";
 export default function Hello() {
   return (
     <header className="hello-section">
-      <img
-        className="header__img"
-        src="http://andreumariner.com/images/andreu-mariner5.jpg"
-        alt="Andreu Mariner"
+      <Img
+        src="https://soythe.best/static/media/andreu.22fa1613.jpg" // 3rd-party image url
       />
       <p>
         <b>Andreu Mariner.</b>
